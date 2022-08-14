8cd529056e8ce74a2dfd03006b87a004ccaea4f9
Sasha Riaboshapka <sashariaboshapka2003@gmail.com>
2022-08-15 02:01:55 +0300

    Deleted unused files

--- app/src/main/java/com/example/animationstestapp/ui/Transition/TransitionToSecondActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.example.animationstestapp.ui.Transition
-
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
-
-interface TransitionToSecondActivity {
-    fun startTransition(context: Context,from: AppCompatActivity ,destination: Class<out Any>, sharedView: android.util.Pair<android.view.View, String>)
-}
